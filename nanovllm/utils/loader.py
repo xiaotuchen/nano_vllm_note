391d550aa31d16c96b6b43b8d04416fb28f27432
xiaotuchen <bing12123@hotmail.com>
2025-09-23 10:41:55 +0800

    add comments to loader.py

--- nanovllm/utils/loader.py
+++ nanovllm/utils/loader.py
@@ -10,14 +10,26 @@ def default_weight_loader(param: nn.Parameter, loaded_weight: torch.Tensor):
 
 
 def load_model(model: nn.Module, path: str):
+    '''in qwen3.py:
+    class Qwen3ForCausalLM(nn.Module):
+    	packed_modules_mapping = {
+    		"q_proj": ("qkv_proj", "q"), # where found key q_proj, its corresponding values are ("qkv_proj", "q")
+    		"k_proi": ("qkv_proj", "k"),
+    		"v_proj": ("qkv_proj", "v"),
+    		"gate_proj": ("gate_up_proj", 0),
+    		"up_proj": ("gate_up_proj", 1),
+    	}
+    '''
+    
     packed_modules_mapping = getattr(model, "packed_modules_mapping", {})
     for file in glob(os.path.join(path, "*.safetensors")):
         with safe_open(file, "pt", "cpu") as f:
             for weight_name in f.keys():
                 for k in packed_modules_mapping:
+                    # print(f"{weight_name}, {f.get_tensor(weight_name).shape}") #to print all param with its shape
                     if k in weight_name:
-                        v, shard_id = packed_modules_mapping[k]
-                        param_name = weight_name.replace(k, v)
+                        v, shard_id = packed_modules_mapping[k] # ie. when k=q_proj, its v=qkv_proj shard_id=q
+                        param_name = weight_name.replace(k, v) # ie. inside weight_name, when found q_proj, replace it with qkv_proj
                         param = model.get_parameter(param_name)
                         weight_loader = getattr(param, "weight_loader")
                         weight_loader(param, f.get_tensor(weight_name), shard_id)
