196144cbc4a35186230cc9fd4adc17a388682215
xiaotuchen <bing12123@hotmail.com>
2025-09-25 15:35:34 +0800

    add comments to rotary_embedding.py

--- nanovllm/layers/rotary_embedding.py
+++ nanovllm/layers/rotary_embedding.py
@@ -8,11 +8,11 @@ def apply_rotary_emb(
     cos: torch.Tensor,
     sin: torch.Tensor,
 ) -> torch.Tensor:
-    cos = cos.unsqueeze(-2)
+    cos = cos.unsqueeze(-2) # [batchSize,1,64], adding the dimension for easy matmul
     sin = sin.unsqueeze(-2)
     x1, x2 = torch.chunk(x.to(torch.float32), 2, dim=-1)
-    y1 = x1 * cos - x2 * sin
-    y2 = x2 * cos + x1 * sin
+    y1 = x1 * cos - x2 * sin # compare to the RoPE arxiv paper equation34, actual implementation, arrange all '-' to y1.  
+    y2 = x2 * cos + x1 * sin # arrange all '+' to y2, these will not change the result.
     return torch.cat((y1, y2), dim=-1).to(x.dtype)
 
 
@@ -26,15 +26,15 @@ class RotaryEmbedding(nn.Module):
         base: float,
     ) -> None:
         super().__init__()
-        self.head_size = head_size
+        self.head_size = head_size # 128
         assert rotary_dim == head_size
-        inv_freq = 1.0 / (base**(torch.arange(0, rotary_dim, 2, dtype=torch.float) / rotary_dim))
-        t = torch.arange(max_position_embeddings, dtype=torch.float)
-        freqs = torch.einsum("i,j -> ij", t, inv_freq)
+        inv_freq = 1.0 / (base**(torch.arange(0, rotary_dim, 2, dtype=torch.float) / rotary_dim)) # 64 elements
+        t = torch.arange(max_position_embeddings, dtype=torch.float) # 40960 elements
+        freqs = torch.einsum("i,j -> ij", t, inv_freq) # freqs.shape=[40960,64], this computes the outer product of t and inv_freq, each element in inv_freq multiply 40960 elements of t. 
         cos = freqs.cos()
         sin = freqs.sin()
-        cache = torch.cat((cos, sin), dim=-1)
-        self.register_buffer("cos_sin_cache", cache, persistent=False)
+        cache = torch.cat((cos, sin), dim=-1) # cache.shape=[40960,128]
+        self.register_buffer("cos_sin_cache", cache, persistent=False) # write cache into buffer, because these values are the same during the model run
 
     @torch.compile
     def forward(
@@ -45,7 +45,7 @@ class RotaryEmbedding(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor]:
         num_tokens = positions.size(0)
         cos_sin = self.cos_sin_cache[positions]
-        cos, sin = cos_sin.chunk(2, dim=-1)
+        cos, sin = cos_sin.chunk(2, dim=-1) # [batchSize,64]
         query_shape = query.shape
         query = query.view(num_tokens, -1, self.head_size)
         query = apply_rotary_emb(query, cos, sin).view(query_shape)
